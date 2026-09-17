c6b6a6e70f75e57518a78efdb31408eafc32f062
norbertown02 <wiederkehrneto@hotmail.com>
2026-09-17 07:33:42 -0300

    style: load isolated clean executive report styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
 import './pro.css'
+import './exec-clean.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
