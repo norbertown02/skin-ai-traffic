43b1bb69af2894792172821bb7ab6f154baa971a
norbertown02 <wiederkehrneto@hotmail.com>
2026-09-17 09:20:01 -0300

    style: load manager and analytics composition layer

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import './pro.css'
 import './exec-clean.css'
 import './skin-brand.css'
 import './app-v2.css'
+import './app-v3.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
