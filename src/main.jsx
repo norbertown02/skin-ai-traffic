8f7b85ce5fb84baa362f2777d3b007339cb3bdda
norbertown02 <wiederkehrneto@hotmail.com>
2026-09-17 08:31:29 -0300

    style: load Skin Beauty brand system across app

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './styles.css'
 import './pro.css'
 import './exec-clean.css'
+import './skin-brand.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
